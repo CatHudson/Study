96ad80dfdedb1a5b8761579f31566119d7069458
Arthur Cherkesov <arturhudson85@gmail.com>
2020-10-16 14:17:08 +0300

    Homework02

--- src/main/java/ru/geekbrains/lesson02/Task05.java
+++ src/main/java/ru/geekbrains/lesson02/Task05.java
@@ -6,26 +6,18 @@ import java.util.Random;
 public class Task05 {
     public static void main(String[] args) {
 
-        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         mix(arr);
         System.out.println(Arrays.toString(arr));
     }
-    public static void mix (int [] arr){
 
-        int min = 0, a=0, c=0;
-        int max = arr.length +1;
-        double b = 0;
-//        double rnd = (Math.random()*((max-min)+1))+min;
-//        int c = (int) rnd;
-        for (int i=0; i<arr.length-1; i++){
-            a=arr[i];
-            b = (Math.random()*((max-min)-1)+min);
-            c = (int) b;
-            arr[i]=arr[c];
-            arr[c] = arr[i];
-            }
+    public static void mix (int [] arr) {
+        Random rnd= new Random();
+        for (int i=0; i<arr.length; i++ ) {
+            int j = rnd.nextInt(arr.length);
+            int x = arr[i];
+            arr[i] = arr[j];
+            arr[j] = x;
         }
-//        for (int i=0; i<arr.length; i++){
-//
-//        }
     }
+}

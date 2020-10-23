d50736d887ed4574fbae3fade2d4c5cb32e0f06d
Arthur Cherkesov <arturhudson85@gmail.com>
2020-10-23 03:02:58 +0300

    Homework05

--- src/main/java/ru/geekbrains/lesson05/Bird.java
+++ src/main/java/ru/geekbrains/lesson05/Bird.java
@@ -1,7 +1,10 @@
 package ru.geekbrains.lesson05;
 
-public class Bird extends Participant {
+public class Bird extends Participant implements AbleToFly {
     public Bird(String name, double flying_length) {
         super(name, 0, 0, 0, flying_length);
+    }
+    public void fly () {
+
     }
 }
